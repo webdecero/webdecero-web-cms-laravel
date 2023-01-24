71029a05fef3b8997c001c5a9da1ad177515ac49
Mayer Abraham Pérez González <38333546+MayerPerez@users.noreply.github.com>
2023-01-24 10:11:28 -0600

    new version front cms

--- src/resources/views/manager/index.blade.php
+++ src/resources/views/manager/index.blade.php
@@ -14,9 +14,11 @@
   <link href="/storage-webcms/manager/css/chunk-05f5db76.73fa10e4.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-149e5792.0a6d4321.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-1555f8f4.4b2cc341.css" rel="prefetch">
-  <link href="/storage-webcms/manager/css/chunk-156a2d30.dcd5cbfd.css" rel="prefetch">
+  <link href="/storage-webcms/manager/css/chunk-1e15ad94.a791bc1f.css" rel="prefetch">
+  <link href="/storage-webcms/manager/css/chunk-1ebbcdae.5c144ef3.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-20c8764d.a1b82a06.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-21b7e1cf.a1b82a06.css" rel="prefetch">
+  <link href="/storage-webcms/manager/css/chunk-387096f1.c0c700c2.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-389483b7.d3ed18ac.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-3a334386.d7d4278b.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-4eb1d91a.68086cc5.css" rel="prefetch">
@@ -25,22 +27,21 @@
   <link href="/storage-webcms/manager/css/chunk-60d07c5e.b143f01a.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-6eb5ec8f.c065d8a9.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-6f3bb142.a1b82a06.css" rel="prefetch">
-  <link href="/storage-webcms/manager/css/chunk-7337c469.95314830.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-9ae600d6.bd661c88.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-a04a5984.ddcd2b2a.css" rel="prefetch">
   <link href="/storage-webcms/manager/css/chunk-c57d3732.b3daab69.css" rel="prefetch">
-  <link href="/storage-webcms/manager/css/chunk-de804e42.b3448c4c.css" rel="prefetch">
+  <link href="/storage-webcms/manager/css/chunk-ca9297f0.19926c54.css" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-03da3084.973dc29b.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-043d756e.9b568418.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-05f5db76.329a2351.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-149e5792.77a1f0c0.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-1555f8f4.048ecfa8.js" rel="prefetch">
-  <link href="/storage-webcms/manager/js/chunk-156a2d30.1d3d16de.js" rel="prefetch">
+  <link href="/storage-webcms/manager/js/chunk-1e15ad94.2cacc6a5.js" rel="prefetch">
+  <link href="/storage-webcms/manager/js/chunk-1ebbcdae.0b1d0f77.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-20c8764d.e30a3dc2.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-21b7e1cf.7b6d8ea8.js" rel="prefetch">
-  <link href="/storage-webcms/manager/js/chunk-2d0a4400.992ec3fc.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-2d0b381b.412d184f.js" rel="prefetch">
-  <link href="/storage-webcms/manager/js/chunk-2d0cf120.d48fd626.js" rel="prefetch">
+  <link href="/storage-webcms/manager/js/chunk-387096f1.0782a165.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-389483b7.0358fd59.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-3a334386.93a43657.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-3e721d4e.59d94eaf.js" rel="prefetch">
@@ -51,14 +52,13 @@
   <link href="/storage-webcms/manager/js/chunk-60d07c5e.3c76a622.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-6eb5ec8f.ff1c7af8.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-6f3bb142.dc9ebb3c.js" rel="prefetch">
-  <link href="/storage-webcms/manager/js/chunk-7337c469.5b0daa55.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-9ae600d6.3d8de7ee.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-a04a5984.1b85cadb.js" rel="prefetch">
   <link href="/storage-webcms/manager/js/chunk-c57d3732.c251b2ff.js" rel="prefetch">
-  <link href="/storage-webcms/manager/js/chunk-de804e42.07cf1c09.js" rel="prefetch">
+  <link href="/storage-webcms/manager/js/chunk-ca9297f0.c9c3445f.js" rel="prefetch">
   <link href="/storage-webcms/manager/css/app.e368c455.css" rel="preload" as="style">
   <link href="/storage-webcms/manager/css/chunk-vendors.91835661.css" rel="preload" as="style">
-  <link href="/storage-webcms/manager/js/app.ba8064d0.js" rel="preload" as="script">
+  <link href="/storage-webcms/manager/js/app.dbf9149b.js" rel="preload" as="script">
   <link href="/storage-webcms/manager/js/chunk-vendors.4c68b457.js" rel="preload" as="script">
   <link href="/storage-webcms/manager/css/chunk-vendors.91835661.css" rel="stylesheet">
   <link href="/storage-webcms/manager/css/app.e368c455.css" rel="stylesheet">
@@ -68,7 +68,7 @@
       continue.</strong></noscript>
   <div id="app"></div>
   <script src="/storage-webcms/manager/js/chunk-vendors.4c68b457.js"></script>
-  <script src="/storage-webcms/manager/js/app.ba8064d0.js"></script>
+  <script src="/storage-webcms/manager/js/app.dbf9149b.js"></script>
 </body>
 
 </html>
